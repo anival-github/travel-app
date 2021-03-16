24be80b403ddab42191089d1abd5b578fe0634c3
Dzianis <imxrayz@gmail.com>
2021-03-17 00:33:58 +0300

    feature: places photos, rating and comments

--- travel-app/src/components/CountryPage/CountryPage.tsx
+++ travel-app/src/components/CountryPage/CountryPage.tsx
@@ -6,11 +6,11 @@ import Photos from './Photos';
 import WidgetsContainer from './Widgets/WidgetsContainer';
 
 const imgUrls = [
-  'http://photoeverywhere.co.uk/west/usa/san_francisco/sunset_sf_skyline6005.jpg',
-  'https://n1s1.elle.ru/ce/df/f6/cedff6cfbc22c90899cddf11f48ac830/940x741_0xc0a839a4_19250053081468488278.jpeg',
-  'https://st.depositphotos.com/1007930/2454/i/600/depositphotos_24543229-stock-photo-hong-kong.jpg',
-  'https://ichef.bbci.co.uk/news/640/cpsprodpb/1356F/production/_106051297_gettyimages-1131483632.jpg',
-  'https://www.iphones.ru/wp-content/uploads/2018/12/345C71E8-2FE2-4362-A8CB-BA600AB33F56.jpeg',
+  { imgUrl: 'https://gls-space.ams3.digitaloceanspaces.com/lbcms-container-cz_excursions_resale/08a1eb18-e094-11ea-8545-baa2e45fb9df.webp', description: 'Description of photo', rating: 5 },
+  { imgUrl: 'https://n1s1.elle.ru/ce/df/f6/cedff6cfbc22c90899cddf11f48ac830/940x741_0xc0a839a4_19250053081468488278.jpeg', description: 'Description of photo', rating: 5 },
+  { imgUrl: 'https://st.depositphotos.com/1007930/2454/i/600/depositphotos_24543229-stock-photo-hong-kong.jpg', description: 'Description of photo', rating: 5 },
+  { imgUrl: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/1356F/production/_106051297_gettyimages-1131483632.jpg', description: 'Description of photo', rating: 5 },
+  { imgUrl: 'https://www.iphones.ru/wp-content/uploads/2018/12/345C71E8-2FE2-4362-A8CB-BA600AB33F56.jpeg', description: 'Description of photo', rating: 5 },
 ];
 
 const CountryPage:React.FC = () => (
