567ef2c95023e946fd3a27fe234f44bc33928316
Dzianis <imxrayz@gmail.com>
2021-03-20 19:31:10 +0300

    feature country-page main info, fix: styles

--- travel-app/src/components/CountryPage/MainInfo.tsx
+++ travel-app/src/components/CountryPage/MainInfo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 type PropsType = {
-  imgUrl:string | null,
-  name:string | null,
+  imgUrl:string,
+  name:string,
   capital:string | null,
   description:string | null,
 };
@@ -11,10 +11,17 @@ const MainInfo:React.FC<PropsType> = ({
   imgUrl, name, capital, description,
 }: PropsType) => (
   <div className="main-info">
-    <span>{name}</span>
-    <span>{capital}</span>
-    <span>{description}</span>
-    <span>{imgUrl}</span>
+    <div className="main-info-img">
+      <img src={imgUrl} alt={name} />
+    </div>
+    <div className="main-info-description">
+      <h1>{name}</h1>
+      <h3>
+        {capital}
+      </h3>
+      <span>{description}</span>
+    </div>
+
   </div>
 );
 
